7873a891dfd0d5bf06c3e5a4bf50607bde4fc1c1
Natalia Portales <portale3@msu.edu>
2024-11-05 13:49:59 -0500

    Setting up repo files

---
+++ scripts/1_practice_script.R
@@ -0,0 +1,38 @@
+###Practice Scripts###
+
+#-----------------------#
+####    Packages     ####
+#-----------------------#
+
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+library(patchwork)
+
+#-----------------------#
+####    Read Data    ####
+#-----------------------#
+
+#Reading in CSV as a tibble
+example.data <- read.csv("../data/raw_data/bad_data_entry_example.csv") %>%
+  as_tibble()
+
+#-----------------------#
+####    Data Manip   ####
+#-----------------------#
+
+unique(example.data$Site.Number)
+
+#clean data set
+clean.example.data <- example.data %>% 
+  na.omit() %>% 
+  rename(site.number = Site.Number) %>% 
+  mutate(
+    site.number = 
+      case_when(site.number == "SITE 1" ~ "1",
+                site.number == "site 3" ~ "3",
+                TRUE ~ site.number))
+
+
+##save outputs as csv
+write.csv(clean.example.data, "../outputs/clean.example.data.csv", row.names = FALSE)
